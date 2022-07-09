a18531f09d67d3a815e3073df6f402d4e45628cb
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-07-09 13:07:38 -0400

    fix: Clear old values after save. (#95)

--- components/ADempiere/TabManager/TabOptions.vue
+++ components/ADempiere/TabManager/TabOptions.vue
@@ -63,8 +63,6 @@ import store from '@/store'
 // components and mixins
 import ActionMenu from '@theme/components/ADempiere/ActionMenu/index.vue'
 import ConvenienceButtons from '@theme/components/ADempiere/TabManager/convenienceButtons.vue'
-// utils and helper methods
-import { isEmptyValue } from '@/utils/ADempiere/valueUtils'
 
 // utils and helper methods
 import { isEmptyValue } from '@/utils/ADempiere/valueUtils'
