c7e9f11dccf7468bfe2b423bdcad06a9ca6a623b
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-07-22 15:39:32 -0400

    Update mixinField.js

--- components/ADempiere/FieldDefinition/mixin/mixinField.js
+++ components/ADempiere/FieldDefinition/mixin/mixinField.js
@@ -131,13 +131,13 @@ export default {
       }
     },
     currentTab() {
-      if (this.isEmptyValue(this.metadata.parentUuid)) {
+      if (this.isEmptyValue(this.metadata.parentUuid) || !this.containerManager.getPanel) {
         return {}
       }
-      return this.$store.getters.getStoredTab(
-        this.metadata.parentUuid,
-        this.metadata.containerUuid
-      )
+      return this.containerManager.getPanel({
+        parentUuid: this.metadata.parentUuid,
+        containerUuid: this.metadata.containerUuid
+      })
     },
     currentRecord() {
       return this.$store.getters.getTabCurrentRow({ containerUuid: this.metadata.containerUuid })
