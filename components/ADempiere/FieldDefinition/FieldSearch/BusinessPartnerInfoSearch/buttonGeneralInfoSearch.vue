91799b1777fb4eb17da85e0d17cdfed6632bd34b
elsiosanchez <elsiosanches@gmail.com>
2022-07-27 13:58:53 -0400

    minimal changes

---
+++ components/ADempiere/FieldDefinition/FieldSearch/BusinessPartnerInfoSearch/buttonGeneralInfoSearch.vue
@@ -0,0 +1,107 @@
+<!--
+ ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+ Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+ Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https:www.gnu.org/licenses/>.
+-->
+
+<template>
+  <el-popover
+    ref="businessParnerListPopover"
+    v-model="showedPopoverGeneralInfoPanel"
+    placement="top"
+    width="900"
+    trigger="click"
+  >
+    <panel-general-info-search
+      v-if="showedPopoverGeneralInfoPanel"
+      :show-popover="showedPopoverGeneralInfoPanel"
+      :metadata="parentMetadata"
+      :container-manager="containerManager"
+    />
+
+    <el-button
+      slot="reference"
+      class="button-search"
+      :disabled="isDisabled"
+    >
+      <svg-icon icon-class="user" />
+    </el-button>
+  </el-popover>
+</template>
+
+<script>
+import store from '@/store'
+
+import PanelGeneralInfoSearch from './panelGeneralInfoSearch.vue'
+
+export default {
+  name: 'GeneralInfoSearch',
+
+  components: {
+    PanelGeneralInfoSearch
+  },
+
+  props: {
+    parentMetadata: {
+      type: Object,
+      default: () => {
+        return {
+          parentUuid: undefined,
+          containerUuid: undefined
+        }
+      }
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false
+    },
+    containerManager: {
+      type: Object,
+      required: true
+    }
+  },
+
+  computed: {
+    uuidForm() {
+      if (!this.isEmptyValue(this.parentMetadata.containerUuid)) {
+        return this.parentMetadata.columnName + '_' + this.parentMetadata.containerUuid
+      }
+      return this.parentMetadata.columnName
+    },
+    showedPopoverGeneralInfoPanel: {
+      get() {
+        return store.getters.getGeneralInfoShow({ containerUuid: this.uuidForm })
+      },
+      set(value) {
+        store.commit('setGeneralInfoShow', {
+          containerUuid: this.uuidForm,
+          show: value
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.button-search {
+  padding-left: 9px !important;
+  padding-right: 0px !important;
+
+  i, svg {
+    font-size: 20px !important;
+  }
+}
+</style>
---
+++ components/ADempiere/FieldDefinition/FieldSearch/InvoiceInfoSearch/index.vue
@@ -0,0 +1,175 @@
+<!--
+ ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+ Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+ Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https:www.gnu.org/licenses/>.
+-->
+
+<template>
+  <el-autocomplete
+    ref="displayBPartner"
+    v-model="displayedValue"
+    v-bind="commonsProperties"
+    value-key="name"
+    clearable
+    style="width: 100%;"
+    popper-class="custom-field-bpartner-info"
+    :trigger-on-focus="false"
+    :fetch-suggestions="localSearch"
+    :select-when-unmatched="false"
+    @select="handleSelect"
+    @clear="clearValues"
+    @focus="setNewDisplayedValue"
+    @blur="setOldDisplayedValue"
+  >
+    <!--
+    @keyup.enter.native="getBPartnerWithEnter"
+      -->
+    <template slot-scope="recordRow">
+      <div class="header">
+        <!-- <span v-for="(identifier, key) in listFilter" :key="key">
+          {{ recordRow.item[identifier.columnName] }}
+        </span> -->
+        {{ recordRow.item[identifier[0].identifier.columnName] }}
+      </div>
+    </template>
+
+    <button-general-info-search
+      slot="append"
+      :parent-metadata="metadata"
+      :is-disabled="isDisabled"
+      :container-manager="containerManager"
+    />
+  </el-autocomplete>
+</template>
+
+<script>
+
+// components and mixins
+import fieldMixin from '@theme/components/ADempiere/FieldDefinition/mixin/mixinField.js'
+import fieldSearchMixin from '@theme/components/ADempiere/FieldDefinition/FieldSearch/mixinFieldSearch.js'
+import generalInfoSearchMixin from './mixinGeneralInfoSearch'
+import ButtonGeneralInfoSearch from './buttonGeneralInfoSearch.vue'
+
+// utils and helper methods
+import { isEmptyValue } from '@/utils/ADempiere/valueUtils'
+
+export default {
+  name: 'GeneralInfoSearch',
+
+  components: {
+    ButtonGeneralInfoSearch
+  },
+
+  mixins: [
+    fieldMixin,
+    fieldSearchMixin,
+    generalInfoSearchMixin
+  ],
+
+  props: {
+    parentMetadata: {
+      type: Object,
+      default: () => {
+        return {
+          containerUuid: ''
+        }
+      }
+    },
+    containerManager: {
+      type: Object,
+      required: true
+    }
+  },
+
+  computed: {
+    // to recrods list overwrite
+    uuidForm() {
+      return this.metadata.containerUuid
+    },
+    listFilter() {
+      const listIdentifier = this.$store.getters.getIdentifier({
+        containerUuid: this.uuidForm
+      })
+      if (this.isEmptyValue(listIdentifier)) {
+        return []
+      }
+      return listIdentifier.filter(identifier => identifier.overwriteDefinition.identifierSequence > 0)
+    }
+  },
+
+  methods: {
+    remoteSearch(searchValue) {
+      return new Promise(resolve => {
+        this.containerManager.generalInfoSearch({
+          containerUuid: this.metadata.containerUuid,
+          pageNumber: 1,
+          tableName: this.metadata.reference.tableName,
+          fieldUuid: this.metadata.uuid,
+          searchValue
+        })
+          .then(responseRecords => {
+            if (isEmptyValue(responseRecords)) {
+              this.whitOutResultsMessage()
+            }
+
+            resolve(responseRecords)
+          })
+          .catch(error => {
+            console.warn(error.message)
+
+            this.whitOutResultsMessage()
+            resolve([])
+          })
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.custom-field-bpartner-info {
+  // button icon suffix
+  .button-search {
+    padding-left: 10px !important;
+    padding-right: 10px !important;
+
+    >i {
+      font-size: 20px;
+    }
+  }
+}
+</style>
+<style lang="scss" scope>
+.custom-field-bpartner-info {
+  // items of lust
+  li {
+    line-height: normal;
+    // padding: 15px;
+    padding-bottom: 5px;
+    padding-top: 5px;
+
+    .header {
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+
+    .info {
+      color: #7e7e7e;
+      float: left;
+      font-size: 12px;
+    }
+  }
+}
+</style>
