84ce067b5c4bc9ca49bec98ab91f1dd01396d07a
elsiosanchez <elsiosanches@gmail.com>
2022-08-23 10:25:43 -0400

    Support to select complete orders

--- components/ADempiere/Form/VPOS/OrderList/fastOrder.vue
+++ components/ADempiere/Form/VPOS/OrderList/fastOrder.vue
@@ -498,24 +498,24 @@ export default {
         const orderUuid = this.$route.query.action
         this.$store.dispatch('listPayments', { posUuid, orderUuid })
       }
-      // if (this.changeOrder.documentStatus.value === 'DR') {
-      holdOrder({
-        posUuid: this.currentPointOfSales.uuid,
-        salesRepresentativeUuid: this.$store.getters['user/getUserUuid'],
-        orderUuid: this.changeOrder.uuid
-      })
-        .then(response => {
-          this.$message.success(this.$t('form.pos.generalNotifications.selectedOrder') + response.documentNo)
+      if (this.changeOrder.documentStatus.value === 'DR') {
+        holdOrder({
+          posUuid: this.currentPointOfSales.uuid,
+          salesRepresentativeUuid: this.$store.getters['user/getUserUuid'],
+          orderUuid: this.changeOrder.uuid
         })
-        .catch(error => {
-          this.$message({
-            message: error.message,
-            isShowClose: true,
-            type: 'error'
+          .then(response => {
+            this.$message.success(this.$t('form.pos.generalNotifications.selectedOrder') + response.documentNo)
           })
-          console.warn(`Error Hold Order ${error.message}. Code: ${error.code}.`)
-        })
-      // }
+          .catch(error => {
+            this.$message({
+              message: error.message,
+              isShowClose: true,
+              type: 'error'
+            })
+            console.warn(`Error Hold Order ${error.message}. Code: ${error.code}.`)
+          })
+      }
       this.clear()
     },
     subscribeChanges() {
