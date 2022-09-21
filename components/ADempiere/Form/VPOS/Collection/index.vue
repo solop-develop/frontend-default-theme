f011a783a84d71d71d8217aa42dd965e2a4a7da4
elsiosanchez <elsiosanches@gmail.com>
2022-09-21 16:12:48 -0400

    remove disable

--- components/ADempiere/Form/VPOS/Collection/index.vue
+++ components/ADempiere/Form/VPOS/Collection/index.vue
@@ -128,7 +128,7 @@
           <samp id="buttonCollection" style="float: right;padding-right: 10px;">
             <el-button type="danger" icon="el-icon-close" @click="exit" />
             <el-button type="info" icon="el-icon-minus" :disabled="isDisabled" @click="undoPatment" />
-            <el-button type="success" icon="el-icon-plus" :disabled="validPay || addPay || validateOpenAmount" @click="addCollectToList(paymentBox)" />
+            <el-button type="success" icon="el-icon-plus" @click="addCollectToList(paymentBox)" />
             <el-button type="primary" icon="el-icon-shopping-cart-full" @click="validateOrder(listPayments)" />
           </samp>
         </el-header>
