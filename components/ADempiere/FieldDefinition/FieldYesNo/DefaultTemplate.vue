32330dcc07c4146e894ed42d04210ff11aa43cd5
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-11-11 16:19:24 -0400

    feat: Advanced Query YesNo field as List field. (#380)

--- components/ADempiere/FieldDefinition/FieldYesNo/DefaultTemplate.vue
+++ components/ADempiere/FieldDefinition/FieldYesNo/DefaultTemplate.vue
@@ -1,7 +1,7 @@
 <!--
  ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
- Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A.
- Contributor(s): Yamel Senih ysenih@erpya.com www.erpya.com
+ Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+ Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
@@ -13,7 +13,7 @@
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with this program.  If not, see <https:www.gnu.org/licenses/>.
+ along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
 
 <template>
@@ -36,7 +36,7 @@ import fieldMixin from '@theme/components/ADempiere/FieldDefinition/mixin/mixinF
 import { convertStringToBoolean } from '@/utils/ADempiere/formatValue/booleanFormat.js'
 
 export default {
-  name: 'FieldYesNo',
+  name: 'FieldYesNoDefault',
 
   mixins: [
     fieldMixin
