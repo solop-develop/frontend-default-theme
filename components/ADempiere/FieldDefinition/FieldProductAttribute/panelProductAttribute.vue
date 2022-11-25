dbd8a0715279cdfbb5e7bec0e5a345be7766d568
elsiosanchez <elsiosanches@gmail.com>
2022-11-25 11:36:44 -0400

    removed

--- components/ADempiere/FieldDefinition/FieldProductAttribute/panelProductAttribute.vue
+++ components/ADempiere/FieldDefinition/FieldProductAttribute/panelProductAttribute.vue
@@ -305,12 +305,6 @@ export default {
         field: this.metadata,
         columnName
       })
-      this.$store.dispatch('flushPersistenceQueue', {
-        parentUuid,
-        containerUuid,
-        tableName: this.metadata.tabTableName,
-        recordUuid: this.$store.getters.getUuidOfContainer(containerUuid)
-      })
     },
     findProductAttributeSetInstace({ productId, id, productAttributeSetId }) {
       getProductAttributeSetInstace({
