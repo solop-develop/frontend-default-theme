37ec00b204b659b34b89891370a5d3dcda6fcead
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-12-30 15:32:06 -0400

    feat: Add copy to clipboard Value and DocumentNo. (#435)
    * feat: Add copy to clipboard Value and DocumentNo.
    
    * fix es lint.

--- components/ADempiere/FieldDefinition/FieldText.vue
+++ components/ADempiere/FieldDefinition/FieldText.vue
@@ -1,7 +1,7 @@
 <!--
  ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
- Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A.
- Contributor(s): Yamel Senih ysenih@erpya.com www.erpya.com
+ Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+ Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
@@ -9,11 +9,11 @@
 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with this program.  If not, see <https:www.gnu.org/licenses/>.
+ along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
 
 <template>
@@ -35,16 +35,27 @@
     @keyup.native="keyReleased"
     @keyup.native.enter="actionKeyPerformed"
     @submit="false"
-  />
+  >
+    <i
+      v-if="isValueField"
+      slot="prefix"
+      class="el-input__icon el-icon-document-copy"
+      @click="copyValue"
+    />
+  </el-input>
 </template>
 
 <script>
-// components and mixins
+// Components and Mixins
 import fieldMixin from '@theme/components/ADempiere/FieldDefinition/mixin/mixinField.js'
 import fieldMixinText from '@theme/components/ADempiere/FieldDefinition/mixin/mixinFieldText.js'
 
-// constants
+// Constants
 import { TEXT } from '@/utils/ADempiere/references'
+
+// Utils and Helper Methods
+import { copyToClipboard } from '@/utils/ADempiere/coreUtils.js'
+import { isEmptyValue } from '@/utils/ADempiere/valueUtils'
 
 export default {
   name: 'FieldText',
@@ -120,6 +131,22 @@ export default {
       }
       // deafult managed with form of fields
       return undefined // 'medium'
+    },
+    isValueField() {
+      return ['DocumentNo', 'Value'].includes(this.metadata.columnName)
+    }
+  },
+
+  methods: {
+    copyValue() {
+      let textToCopy = this.value
+      if (isEmptyValue(textToCopy)) {
+        textToCopy = '' // empty string
+      }
+      copyToClipboard({
+        text: textToCopy,
+        isShowMessage: true
+      })
     }
   }
 }
