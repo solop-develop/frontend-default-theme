9222c969c126591e2ad89a2e9170f8a2df04d641
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2023-01-31 21:48:30 -0400

    feat: Loading save sequence. (#503)

--- components/ADempiere/PanelDefinition/DraggablePanel.vue
+++ components/ADempiere/PanelDefinition/DraggablePanel.vue
@@ -1,19 +1,19 @@
 <!--
-ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
-Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
-Contributor(s): Elsio Sanchez Elsiosanches@gmail.com https://github.com/elsiosanchez
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+ ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+ Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+ Contributor(s): Elsio Sanchez Elsiosanches@gmail.com https://github.com/elsiosanchez
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https:www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
 
 <template>
@@ -37,7 +37,9 @@ along with this program. If not, see <https:www.gnu.org/licenses/>.
             :new-fields-list-secuence="draggableList"
             :fields-customization="fieldsAttributesCustomization"
           />
+
           <el-card
+            v-loading="isLoadingSaveCustomization"
             :shadow="shadowGroup"
             :body-style="{ padding: '5px' }"
             :class="isActiveCurrentTab ? 'custom-panel-field' : ''"
@@ -147,6 +149,14 @@ export default defineComponent({
         return true
       }
       return false
+    })
+
+    const isLoadingSaveCustomization = computed(() => {
+      const { isLoadedFieldsList } = props.containerManager.getPanel({
+        parentUuid: props.parentUuid,
+        containerUuid: props.containerUuid
+      })
+      return !isLoadedFieldsList
     })
 
     const fieldsList = computed(() => {
@@ -306,6 +316,7 @@ export default defineComponent({
       //
       fieldsAttributesCustomization,
       fieldsList,
+      isLoadingSaveCustomization,
       sortColumnName,
       // oldFieldsList,
       shadowGroup,
@@ -313,7 +324,7 @@ export default defineComponent({
       styleScrollPanelTab,
       isMobile,
       isActiveCurrentTab,
-      // methodos
+      // methods
       handleChange
     }
   }
