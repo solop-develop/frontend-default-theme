5a57f6d7198f6b76b4215b58b1871745b8d31441
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2023-05-31 14:35:48 -0400

    Update AutomaticMatch.vue

--- components/ADempiere/Form/VBankStatementMatch/AutomaticMatch.vue
+++ components/ADempiere/Form/VBankStatementMatch/AutomaticMatch.vue
@@ -1,6 +1,6 @@
 <!--
 ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
-Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A.
+Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A.
 Contributor(s): Elsio Sanchez elsiosanchez15@outlook.com https://github.com/elsiosanchez
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -9,12 +9,13 @@ the Free Software Foundation, either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https:www.gnu.org/licenses/>.
+along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
+
 <template>
   <div style="height: 100% !important;">
     <el-card id="panel-top-search-criteria" class="panel-top-search-criteria" style="height: 100% !important;">
@@ -64,10 +65,12 @@ along with this program.  If not, see <https:www.gnu.org/licenses/>.
 
 <script>
 import { defineComponent, computed, ref } from '@vue/composition-api'
+
 import store from '@/store'
 
 export default defineComponent({
   name: 'AutomaticMatch',
+
   props: {
     title: {
       type: String,
@@ -78,11 +81,11 @@ export default defineComponent({
       default: true
     }
   },
+
   setup(props, { root }) {
     /**
     * Refs
     */
-
     const matchingMovements = computed(() => {
       return store.getters.getListMatchingMovements
     })
