f0db25d7fd24411e1deae230f9cfd7939d408a59
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2023-06-20 15:23:28 -0400

    Fix: Field Text Long change value (#652)
    * Fix: Field Text Long
    
    * Fix Construction
    
    * Update FieldTextLong.vue
    
    ---------
    
    Co-authored-by: Edwin Betancourt <EdwinBetanc0urt@outlook.com>

--- components/ADempiere/FieldDefinition/FieldTextLong.vue
+++ components/ADempiere/FieldDefinition/FieldTextLong.vue
@@ -1,6 +1,6 @@
 <!--
  ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
- Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+ Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
  Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -29,11 +29,15 @@
     v-model="value"
     left-toolbar="clear h bold italic strikethrough quote ul ol table hr link image | emoji listMailTemplates"
     height="250px"
-    @change="preHandleChange"
   />
 </template>
 
 <script>
+import store from '@/store'
+
+// Utils and Helper Methods
+import { isEmptyValue } from '@/utils/ADempiere/valueUtils'
+
 // Components and Mixins
 import fieldMixin from '@theme/components/ADempiere/FieldDefinition/mixin/mixinField.js'
 import fieldMixinText from '@theme/components/ADempiere/FieldDefinition/mixin/mixinFieldText.js'
@@ -61,6 +65,67 @@ export default {
         styleClass += ' custom-field-text-long-disable '
       }
       return styleClass
+    },
+    value: {
+      get() {
+        const { columnName, containerUuid, inTable } = this.metadata
+        // table records values
+        if (inTable) {
+          // implement container manager row
+          if (this.containerManager && this.containerManager.getCell) {
+            const value = this.containerManager.getCell({
+              containerUuid,
+              rowIndex: this.metadata.rowIndex,
+              columnName
+            })
+            if (!isEmptyValue(value)) {
+              return value
+            }
+          }
+        }
+
+        const valueEditor = store.getters.getValueOfFieldOnContainer({
+          parentUuid: this.metadata.parentUuid,
+          containerUuid,
+          columnName
+        })
+
+        if (this.isEmptyValue(valueEditor)) return ''
+        return valueEditor
+      },
+      set(newValue) {
+        const { columnName, containerUuid, inTable } = this.metadata
+
+        // table records values
+        if (inTable) {
+          // implement container manager row
+          if (this.containerManager && this.containerManager.setCell) {
+            this.containerManager.setCell({
+              containerUuid,
+              rowIndex: this.metadata.rowIndex,
+              columnName,
+              value: newValue
+            })
+          }
+        }
+
+        store.commit('updateValueOfField', {
+          parentUuid: this.metadata.parentUuid,
+          containerUuid,
+          columnName,
+          value: newValue
+        })
+        // update element column name
+        if (!this.metadata.isSameColumnElement) {
+          store.commit('updateValueOfField', {
+            parentUuid: this.metadata.parentUuid,
+            containerUuid,
+            columnName: this.metadata.elementName,
+            value: newValue
+          })
+        }
+        this.preHandleChange(newValue)
+      }
     }
   }
 }
