f7e2d4a5e94e9727089696345a2653bd6fcb0f9e
Elsio Sanchez <elsiosanches@gmail.com>
2023-07-12 16:54:14 -0400

    remove console

--- components/ADempiere/Form/index.vue
+++ components/ADempiere/Form/index.vue
@@ -37,7 +37,6 @@ export default {
     // load the component that is indicated in the attributes of received property
     componentRender() {
       let form
-      console.log(this.metadata.fileName)
       switch (this.metadata.fileName) {
         case 'PriceChecking':
           form = import('@theme/components/ADempiere/Form/PriceChecking')
