0094ba61596e6539b93bd91cec7b7a6a6faa9ef3
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2023-07-23 22:53:35 -0400

    fix: Report field date is rangue without picker range. (#735)

--- components/ADempiere/FieldDefinition/FieldDate.vue
+++ components/ADempiere/FieldDefinition/FieldDate.vue
@@ -1,19 +1,19 @@
 <!--
- ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
- Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
- Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+  ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+  Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+  Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <https:www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
 
 <template>
@@ -148,6 +148,10 @@ export default {
   },
 
   computed: {
+    isMultipleValues() {
+      return !isEmptyValue(this.metadata.operator) &&
+        MULTIPLE_VALUES_OPERATORS_LIST.includes(this.metadata.operator)
+    },
     isRenderRange() {
       if (!isEmptyValue(this.metadata.operator)) {
         return RANGE_VALUE_OPERATORS_LIST.includes(this.metadata.operator)
@@ -171,10 +175,6 @@ export default {
     },
     cssClassCustomField() {
       return ' custom-field-date '
-    },
-    isMultipleValues() {
-      return !isEmptyValue(this.metadata.operator) &&
-        MULTIPLE_VALUES_OPERATORS_LIST.includes(this.metadata.operator)
     },
     /**
      * Parse the date format to be compatible with element-ui
