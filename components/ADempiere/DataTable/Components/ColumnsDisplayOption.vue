12f4c3590815fe7c3e560c110b15ba96493f0489
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2023-07-24 18:58:34 -0400

    Fix:  Change the Table Display (#729)
    * Fix:  Change the Table Display
    
    * remove console.log
    
    ---------
    
    Co-authored-by: EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>

--- components/ADempiere/DataTable/Components/ColumnsDisplayOption.vue
+++ components/ADempiere/DataTable/Components/ColumnsDisplayOption.vue
@@ -1,6 +1,6 @@
 <!--
  ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
- Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+ Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
  Contributor(s): Elsio Sanchez elsiosanches@gmail.com https://github.com/elsiosanchez
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -9,11 +9,11 @@
 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with this program.  If not, see <https:www.gnu.org/licenses/>.
+ along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
 
 <template>
@@ -25,6 +25,7 @@
     <el-dropdown-menu slot="dropdown" style="max-width: 300px;">
       <el-dropdown-item
         :command="{
+          command: 'showMinimalistView',
           dispatch: 'selectOption',
           value: $t('table.dataTable.showMinimalistView')
         }"
@@ -35,6 +36,7 @@
       <el-dropdown-item
         :command="{
           dispatch: 'selectOption',
+          command: 'showAllColumns',
           value: $t('table.dataTable.showAllColumns')
         }"
       >
@@ -44,6 +46,7 @@
       <el-dropdown-item
         :command="{
           dispatch: 'selectOption',
+          command: 'showOnlyMandatoryColumns',
           value: $t('table.dataTable.showOnlyMandatoryColumns')
         }"
       >
@@ -53,6 +56,7 @@
       <el-dropdown-item
         :command="{
           dispatch: 'selectOption',
+          command: 'showTableColumnsOnly',
           value: $t('table.dataTable.showTableColumnsOnly')
         }"
       >
@@ -79,6 +83,26 @@ export default defineComponent({
     containerUuid: {
       type: String,
       required: false
+    },
+    availableFields: {
+      type: Array,
+      required: true
+    },
+    availableFieldsWithValue: {
+      type: Array,
+      required: true
+    },
+    filterManager: {
+      type: Function,
+      default: ({ filterList }) => {}
+    },
+    fieldsToHidden: {
+      type: Function,
+      default: ({ filterList }) => { return [] }
+    },
+    allFieldsList: {
+      type: Array,
+      required: true
     }
   },
 
@@ -94,12 +118,59 @@ export default defineComponent({
       return 'eye'
     }
 
-    const handleCommand = (command) => {
-      store.dispatch(command.dispatch, {
+    const handleCommand = (options) => {
+      const { command, dispatch, value } = options
+
+      // Change Name of Selected Option
+      store.dispatch(dispatch, {
         parentUuid: props.parentUuid,
         containerUuid: props.containerUuid,
-        tableOption: command.value
+        tableOption: value
       })
+
+      // set columns to show/hidden in vuex store
+      const fieldsShowed = listDisplay(command)
+      // if (isEmptyValue(fieldsShowed)) return
+
+      props.filterManager({
+        parentUuid: props.parentUuid,
+        containerUuid: props.containerUuid,
+        fieldsShowed,
+        fieldsList: props.availableFields
+      })
+    }
+
+    /**
+     * List to Show According to the Selected Option
+     * @param {String} option - selected option
+     * @param {Array} listShowed - List Show
+     * @returns {Array} List of Columns to Show
+     */
+
+    function listDisplay(option, listShowed = []) {
+      switch (option) {
+        case 'showAllColumns':
+          listShowed = props.allFieldsList.map(list => {
+            return list.columnName
+          })
+          break
+        case 'showOnlyMandatoryColumns':
+          listShowed = props.allFieldsList
+            .filter(list => list.isMandatory)
+            .map(list => list.columnName)
+          break
+        case 'showTableColumnsOnly':
+          listShowed = props.allFieldsList
+            .filter(list => list.isDisplayedGrid)
+            .map(list => list.columnName)
+          break
+        case 'showMinimalistView':
+          listShowed = props.allFieldsList
+            .filter(field => field.isDisplayed && field.isShowedFromUser)
+            .map(field => field.columnName)
+          break
+      }
+      return listShowed
     }
 
     return {
