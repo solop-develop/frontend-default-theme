4c3cc3ee3a19e0b4a51d3a8c1d35dc07514d581a
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2023-08-18 19:47:28 -0400

    Fix: Allows you to make a return of a draft order (#787)

--- components/ADempiere/Form/VPOS/Options/index.vue
+++ components/ADempiere/Form/VPOS/Options/index.vue
@@ -875,7 +875,13 @@ export default {
       return ''
     },
     allowsReturnOrder() {
-      return this.$store.getters.posAttributes.currentPointOfSales.isAllowsReturnOrder
+      const { isAllowsReturnOrder } = this.$store.getters.posAttributes.currentPointOfSales
+      if (isAllowsReturnOrder) {
+        if (this.currentOrder.documentStatus.value === 'CO') return true
+        return false
+      }
+      return isAllowsReturnOrder
+      // return this.$store.getters.posAttributes.currentPointOfSales.isAllowsReturnOrder
     },
     allowsCreateOrder() {
       return this.$store.getters.posAttributes.currentPointOfSales.isAllowsCreateOrder
